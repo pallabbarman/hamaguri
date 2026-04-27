578ff4a3c4036d84205f74d655de9c7a9e615214
Pallab Bharman <bharmanpallab@gmail.com>
2026-04-27 13:22:11 +0600

    navbar base added

--- src/layouts/WebsiteLayout/components/WebsiteLayoutNavbar/index.tsx
+++ src/layouts/WebsiteLayout/components/WebsiteLayoutNavbar/index.tsx
@@ -1,32 +1,15 @@
+import { Box, Container, Typography } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import MenuIcon from 'icons/MenuIcon';
 
 const WebsiteLayoutNavbar = () => {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" sx={{ bgcolor: 'white.main' }}>
-                <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h3" component="div" sx={{ flexGrow: 1 }}>
-                        Hamaguri
-                    </Typography>
-                    <Button color="inherit">Login</Button>
-                </Toolbar>
-            </AppBar>
-        </Box>
+        <AppBar position="sticky" sx={{ bgcolor: 'white.main', boxShadow: 'none', py: 2 }}>
+            <Container maxWidth="xl">
+                <Box>
+                    <Typography variant="h2">Hamaguri</Typography>
+                </Box>
+            </Container>
+        </AppBar>
     );
 };
 
